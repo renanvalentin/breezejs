d5e5f09dacc5732786ad96236a32afa458219f16
Renan <renan.lifestyle@hotmail.com>
2013-07-11 22:33:15 -0300

    netbiis project

---
+++ NetBiis/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Optimization;
+using BundleTransformer.Core.Transformers;
+using BundleTransformer.Core.Builders;
+
+namespace NetBiis.App_Start
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery",
+                  "//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js")
+                  .Include("~/Scripts/jquery-{version}.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            var lessBundle = new Bundle("~/bundle/less").Include(
+                "~/Content/less/core.less"
+            );
+            lessBundle.Builder = new NullBuilder();
+            lessBundle.Transforms.Add(new CssTransformer());
+            bundles.Add(lessBundle);
+        }
+    }
+}
